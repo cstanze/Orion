f929ea16a735b19aa4390e5c88cb28ba64d984b0
Constanze <jules.nieves0301@gmail.com>
2022-01-15 18:53:07 -0500

    Initial Commit

---
+++ Orion/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Orion
+//
+//  Created by Jules Amalie on 2022/01/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
